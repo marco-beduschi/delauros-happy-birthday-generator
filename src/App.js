76e43e65b8e6d890f011967bb884e7e04e037889
Marco Beduschi <beduschimarco@gmail.com>
2021-09-02 19:34:29 -0300

    refactor: format code with prettier

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import {useEffect, useState} from "react";
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 import { MESSAGES } from "./messages";
 
 function App() {
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     if (messages.length === MESSAGES.length) {
-        return null;
+      return null;
     }
 
-    const newMessages = [...messages, MESSAGES[(MESSAGES.length - 1) - (messages.length)]]
-    setTimeout(() => setMessages(newMessages), Math.random() * 2 * 1000)
-  }, [messages])
+    const newMessages = [
+      ...messages,
+      MESSAGES[MESSAGES.length - 1 - messages.length],
+    ];
+    setTimeout(() => setMessages(newMessages), Math.random() * 2 * 1000);
+  }, [messages]);
 
   return (
     <div className="App">
-      <div className="Header"/>
+      <div className="Header" />
       <div className="Body">
-          <div className="ChatRoom">
-              {
-                  messages.map((msg) => {
-                      return <div className="Message">
-                          <div className="MessageHeader">
-                              <p>{msg.sender}</p>
-                          </div>
-                          <div className="MessageContent">
-                              <p>
-                                  {msg.content}
-                              </p>
-                          </div>
-                      </div>
-                  })
-              }
-          </div>
+        <div className="ChatRoom">
+          {messages.map((msg) => {
+            return (
+              <div className="Message">
+                <div className="MessageHeader">
+                  <p>{msg.sender}</p>
+                </div>
+                <div className="MessageContent">
+                  <p>{msg.content}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/messages.js
+++ src/messages.js
@@ -1,90 +1,200 @@
 export const MESSAGES = [
-    {
-        sender: "Teci",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Guilherme Vieira",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Marco Beduschi",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Henrique",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "João V",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Luísa Danzberg",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Mini Bel",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Naná",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Pedreiro",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Amanda Barni",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Fred",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Ivhan",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "João Caetano",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Rafilska",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Téci",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Volkmann",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Gabriela Pinheiro",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Mariana Clara",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Luis Roberto G",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Deco",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Fábio Luís",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-    {
-        sender: "Alexandre",
-        content: <p>parabéns Nobre Doutor adÊvogado @<span className="mention">Guilherme Vieira</span></p>
-    },
-]
+  {
+    sender: "Teci",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Guilherme Vieira",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Marco Beduschi",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Henrique",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "João V",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Luísa Danzberg",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Mini Bel",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Naná",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Pedreiro",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Amanda Barni",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Fred",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Ivhan",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "João Caetano",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Rafilska",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Téci",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Volkmann",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Gabriela Pinheiro",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Mariana Clara",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Luis Roberto G",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Deco",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Fábio Luís",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+  {
+    sender: "Alexandre",
+    content: (
+      <p>
+        parabéns Nobre Doutor adÊvogado @
+        <span className="mention">Guilherme Vieira</span>
+      </p>
+    ),
+  },
+];
